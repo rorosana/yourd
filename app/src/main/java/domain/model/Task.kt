028b7ea4e2f043e302c7d20084d7d6af716c7ac6
Rosana <rosana.patino@es.schunk.com>
2025-12-28 21:43:44 +0100

    Add Task domain model

---
+++ app/src/main/java/domain/model/Task.kt
@@ -0,0 +1,9 @@
+package com.rosana.yourdead.domain.model
+
+data class Task(
+    val id: Int = 0,
+    val title: String,
+    val description: String = "",
+    val isCompleted: Boolean = false,
+    val createdAt: Long = System.currentTimeMillis()
+)
